956fd9e7d91549b47eae1de64dcd63d9af6d8d61
rlstallard <89097824+rlstallard@users.noreply.github.com>
2023-04-30 14:14:00 -0400

    Update over25Tickets.js

--- over25Tickets.js
+++ over25Tickets.js
@@ -1,9 +1,7 @@
  let app = Vue.createApp({
       data() {
           return {
-              tickets: [],	//holds all tickets
-			  over25Tickets: []	//holds over 25 mph tickets
-			  
+              tickets: []	//holds 25 mph and over tickets
           }
       },
       created() {
@@ -12,10 +10,9 @@
       methods: {    // ajax request
           async getData() {
               const response = await fetch('tickets1.json');
-              this.tickets = await response.json();
+              const allTickets = await response.json();
+			  this.tickets = allTickets.filter( (ticket) => ((ticket.actual_speed - ticket.posted_speed) >= 25) );
 			  console.log(this.tickets[0]);
-			  this.over25tickets = tickets.filter( (ticket) => ((ticket.actual_speed - ticket.posted_speed) >= 25) );
-			  console.log(this.over25Tickets[0]);
           },
       }
     });
